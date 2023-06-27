a7d696a064aa93997272691c20f9990dcf5ccac9
Vladyslav Tereshchuk <vtereshchuk21@gmail.com>
2023-06-27 19:29:20 +0300

    react-app init

---
+++ TimeTracker/ClientApp/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+ 
+
+    return (
+        <div>
+           <h1 style={{textAlign:'center'}}>React</h1>
+            <h2>App</h2>
+        </div>
+    );
+}
+
+export default App;
--- TimeTracker/Program.cs
+++ TimeTracker/Program.cs
@@ -1,10 +1,42 @@
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSpaStaticFiles(conf =>
+{
+    conf.RootPath = "ClientApp/build";
+});
 var app = builder.Build();
 
 
-app.Map("/", async (c) =>
+app.UseSpaStaticFiles();
+
+app.UseSpa(spa =>
 {
-   await c.Response.WriteAsync("Hello");
+    spa.Options.SourcePath = "ClientApp";
+    spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions()
+    {
+        OnPrepareResponse = ctx =>
+        {
+            var headers = ctx.Context.Response.GetTypedHeaders();
+
+            headers.CacheControl = new CacheControlHeaderValue()
+            {
+                 NoCache = true,
+                 NoStore = true,
+                 MustRevalidate = true
+            };
+
+        }
+    };
+    
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseReactDevelopmentServer(npmScript:"start");
+    }
+
 });
+
 
 app.Run();
