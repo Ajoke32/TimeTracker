edd5b9db43396c2671a282af8734981b918e822b
Ajoke32 <ipz211_bmm@student.ztu.edu.ua>
2023-07-19 18:31:24 +0300

    query fixed

--- TimeTracker/ClientApp/src/redux/queries/vacationApproverQueries.ts
+++ TimeTracker/ClientApp/src/redux/queries/vacationApproverQueries.ts
@@ -7,7 +7,7 @@ import {ApproverVacationUpdate} from "../types/approverVacationTypes";
 export function UpdateApproverVacationState(id:number,state:boolean){
     return AjaxQuery<QueryStructure<{ approverVacationMutation: { updateState:ApproverVacationUpdate}}>>(
         'mutation UpdateApproverVacationState($state:Boolean!,$id:Int!)' +
-        '{approverVacationMutation{updateStater(state:$state,vacationId:$id){id,isApproved,vacation{id}}}}',
+        '{approverVacationMutation{updateState(state:$state,vacationId:$id){id,isApproved,vacation{id}}}}',
         {id:id,state:state}
     )
 }
