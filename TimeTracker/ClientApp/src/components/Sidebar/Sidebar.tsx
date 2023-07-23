f36d86b3f16c8d35b1a0dbffc994e04c9062e814
Ajoke32 <ipz211_bmm@student.ztu.edu.ua>
2023-07-23 12:20:05 +0300

    routes changed

--- TimeTracker/ClientApp/src/components/Sidebar/Sidebar.tsx
+++ TimeTracker/ClientApp/src/components/Sidebar/Sidebar.tsx
@@ -26,7 +26,7 @@ export const Sidebar = () => {
                     </li>
 
                     <li>
-                        <a href="/vacations" className="sidebar-list__link">
+                        <a href="/vacation/all" className="sidebar-list__link">
                             <div className="sidebar-list__image-wrapper"><div className="timetracker-img__wrapper"></div></div>
                             <span>My vacations</span>
                         </a>
@@ -56,7 +56,7 @@ export const Sidebar = () => {
                     </li>
 
                     <li>
-                        <a href="" className="sidebar-list__link">
+                        <a href="#" className="sidebar-list__link">
                             <div className="sidebar-list__image-wrapper"><div className="settings-img__wrapper"></div></div>
                             <span>Settings</span>
                         </a>
