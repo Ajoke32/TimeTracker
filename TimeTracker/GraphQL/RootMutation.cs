befe0fb82090e2ffe5e7c2fa0aae0586ae1ece08
Ajoke32 <ipz211_bmm@student.ztu.edu.ua>
2023-08-04 22:15:59 +0300

    removed unused mutation

--- TimeTracker/GraphQL/RootMutation.cs
+++ TimeTracker/GraphQL/RootMutation.cs
@@ -19,10 +19,7 @@ public sealed class RootMutation:ObjectGraphType
         Field<ApproverVacationMutations>("approverVacationMutation")
             .Resolve(_ => new { });
         
-        Field<WorkedHoursMutations>("WorkedHoursMutations")
+        Field<WorkedHourMutations>("WorkedHoursMutations")
             .Resolve(_ => new { });
-        
-        Field<CalendarEventMutations>("calendarEventMutation")
-
     }
 }
