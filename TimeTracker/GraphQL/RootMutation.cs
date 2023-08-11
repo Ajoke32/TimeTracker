91d948c0c09febd766f87731d0163b155e448b8a
Ajoke32 <ipz211_bmm@student.ztu.edu.ua>
2023-08-11 14:50:08 +0300

    root mutation

--- TimeTracker/GraphQL/RootMutation.cs
+++ TimeTracker/GraphQL/RootMutation.cs
@@ -19,10 +19,10 @@ public sealed class RootMutation:ObjectGraphType
         Field<ApproverVacationMutations>("approverVacationMutation")
             .Resolve(_ => new { });
         
-        Field<WorkedHoursMutations>("WorkedHoursMutations")
+        Field<WorkedHourMutations>("WorkedHoursMutations")
             .Resolve(_ => new { });
-        
-        Field<CalendarEventMutations>("calendarEventMutation")
+
+        /*Field<CalendarEventMutations>("calendarEventMutation");*/
 
     }
 }
