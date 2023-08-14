b6ce9f5cfac388f47bfbeb7db782dbbadd98df47
Vladyslav Tereshchuk <vtereshchuk21@gmail.com>
2023-08-14 14:46:03 +0300

    Error fix

--- TimeTracker/ClientApp/src/redux/intrerfaces/queryStructure.ts
+++ TimeTracker/ClientApp/src/redux/intrerfaces/queryStructure.ts
@@ -1,12 +1,13 @@
 export interface QueryStructure<T> {
     data: T,
     errors: ResponseError[] | null,
+    extensions: { count: number }
 }
 
-export interface ResponseError{
-    message:string,
-    extensions:{
-        code:string,
-        details:string
+export interface ResponseError {
+    message: string,
+    extensions: {
+        code: string,
+        details: string
     }
 }
