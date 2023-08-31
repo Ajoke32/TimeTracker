31ca4e33f8e0f256aa2afc9f288090ce42bd131c
Vladyslav Tereshchuk <vtereshchuk21@gmail.com>
2023-08-31 22:30:56 +0300

    Deleted logic that was moved to dashboard

--- TimeTracker/ClientApp/src/pages/TrackerPage/Tracker.tsx
+++ TimeTracker/ClientApp/src/pages/TrackerPage/Tracker.tsx
@@ -9,37 +9,10 @@ import { GetFormattedDateString, GetOneMonthDateRange } from "../../utils";
 
 export const Tracker = () => {
 
-    const dispatch = useAppDispatch();
-    const { user } = useTypedSelector(state => state.auth);
-    const { loading, workedHours,
-        take, skip, extensions, perPage } = useTypedSelector(state => state.workedHours)
-
-    useEffect(() => {
-        dispatch(fetchWorkedHours({
-            userId: user!.id,
-            dateRange: GetOneMonthDateRange(new Date())
-        }))
-    }, [take, skip])
-
     return (
         <div style={{ marginTop: '20px' }}>
             <div className='tracker-page-wrapper'>
                 <TimeTracker />
-                <div className="trackers-separator__wrapper">
-                    <hr className="horizontal-line " />
-                </div>
-                {loading
-                    ?
-                    <div className="loader-wrapper">
-                        <Loader />
-                    </div>
-                    : workedHours.map((wh) => (
-                        <TimeTracker workedHour={wh} key={wh.id} />
-                    ))
-                }
-                {extensions?.count! > perPage &&
-                    <Pager capacity={2} take={take} skip={skip} perPage={perPage} setTake={setWorkedHoursTake}
-                        setSkip={setWorkedHourSkip} extensions={{ count: extensions?.count! }} />}
             </div>
         </div >
     );
