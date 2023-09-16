0ad908dd990ac966f05c5273b75f2a8e0428d227
Ajoke32 <ipz211_bmm@student.ztu.edu.ua>
2023-09-16 16:19:47 +0300

    password recovery logic changed

--- TimeTracker/ClientApp/src/components/PasswordRecoveryForms/StepForm.tsx
+++ TimeTracker/ClientApp/src/components/PasswordRecoveryForms/StepForm.tsx
@@ -4,11 +4,12 @@ interface FirstStepFormProps{
     setValue:(value:string)=>void,
     dispatchFunction:()=>void,
     value:string,
-    placeHolder:string
+    placeHolder:string,
+    isLastStep:boolean
 }
 
 
-const StepForm = ({setValue,dispatchFunction,value,placeHolder}:FirstStepFormProps) => {
+const StepForm = ({setValue,dispatchFunction,value,placeHolder,isLastStep}:FirstStepFormProps) => {
 
     function onInputEmailChange(e:React.ChangeEvent<HTMLInputElement>){
         setValue(e.target.value);
@@ -17,7 +18,7 @@ const StepForm = ({setValue,dispatchFunction,value,placeHolder}:FirstStepFormPro
     return (
         <div style={{display:"flex",gap:"8px",flexDirection:"column"}}>
             <input onChange={(e)=>onInputEmailChange(e)} value={value} className="search-input" type="text" placeholder={placeHolder}/>
-            <button onClick={dispatchFunction} className="btn-base btn-info">next</button>
+            <button onClick={dispatchFunction} className="btn-base btn-info">{!isLastStep?"next":"create"}</button>
         </div>
     );
 };
--- TimeTracker/ClientApp/src/pages/PasswordRecoveryPage/PasswordRecovery.tsx
+++ TimeTracker/ClientApp/src/pages/PasswordRecoveryPage/PasswordRecovery.tsx
@@ -7,18 +7,34 @@ import {codeVerify, createPassword, resetPassword} from "@redux/slices";
 const formPlaceHolders = ['enter you email','enter code','come up with a new password']
 
 const stepMessages = [formPlaceHolders[0],'The code was sent to the email','Create new a new password']
+
+const loadingMessages = ['sending code to you email...','password checking...','password creation...']
+
 const PasswordRecovery = () => {
 
     const dispatch = useAppDispatch();
 
 
-    const {isEmailConfirmationDelivered,userId,isCodeMatch,message,error} = useTypedSelector(s=>s.auth);
+    const {isEmailConfirmationDelivered,loading,userId,isCodeMatch,message,error} = useTypedSelector(s=>s.auth);
 
     const [value,setFormValue] = useState<string>("");
 
     const [step,setStep] = useState<number>(0);
 
     const dispatchFunctions = [handlePasswordRecovery,handleCodeVerify,handlePasswordCreate];
+
+
+    useEffect(() => {
+        if(isEmailConfirmationDelivered){
+            setStep(1);
+            setFormValue("");
+        }
+        if(isCodeMatch){
+            setStep(2);
+            setFormValue("");
+        }
+    }, [isEmailConfirmationDelivered,isCodeMatch]);
+
 
     function handlePasswordRecovery(){
         dispatch(resetPassword(value));
@@ -29,11 +45,6 @@ const PasswordRecovery = () => {
         if(userId!==null){
             dispatch(codeVerify({userId:userId,
             code:value}))
-            console.log(isCodeMatch);
-            if(isCodeMatch){
-                setStep(2);
-                setFormValue("");
-            }
         }
     }
 
@@ -45,20 +56,14 @@ const PasswordRecovery = () => {
     }
 
 
-    useEffect(() => {
-        if(isEmailConfirmationDelivered){
-            setStep(1);
-            setFormValue("");
-        }
-    }, [isEmailConfirmationDelivered]);
-
-
     return (
         <div className="password-recovery-wrapper">
             <div className="password-recovery-wrapper-inner">
                 <h1 style={{textAlign:"center"}}>{step===0?"Password recovery":stepMessages[step]}</h1>
-                <h1 style={{textAlign:"center",color:"red"}}>{error&&error} {message&&message}</h1>
-               <StepForm placeHolder={formPlaceHolders[step]} setValue={setFormValue}
+                {error!==null||message!==null
+                    &&<h1 style={{textAlign:"center",color:`${error?'red':"#a2d2ff"}`}}>{error} {message}</h1>}
+                {loading&&<h1 style={{textAlign:"center"}}>{loadingMessages[step]}</h1>}
+               <StepForm isLastStep={step===2} placeHolder={formPlaceHolders[step]} setValue={setFormValue}
                          value={value}
                          dispatchFunction={dispatchFunctions[step]} />
             </div>
